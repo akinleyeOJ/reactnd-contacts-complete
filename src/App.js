e9ee31aa380a03a1dd9176c09d9bb82f04accf63
OLUWATIMILEHIN AKINLEYE <akinleyeoj@gmail.com>
2020-06-23 13:06:17 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,24 @@ import * as ContactsAPI from './utils/ContactsAPI'
 class App extends Component {
   state = {
     contacts: [
-      
+        {
+          "id": "karen",
+          "name": "Karen Isgrigg",
+          "handle": "karen_isgrigg",
+          "avatarURL": "http://localhost:5001/karen.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "richardkalehoff",
+          "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "tylermcginnis",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
     ]
   }
   componentDidMount() {
